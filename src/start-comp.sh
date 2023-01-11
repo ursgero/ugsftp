bbc0dc58ec87404434bf40a34ace04e215f1a1c0
ursgero <121879443+ursgero@users.noreply.github.com>
2023-01-11 11:54:01 +0100

    Update start-comp.sh

--- src/start-comp.sh
+++ src/start-comp.sh
@@ -1,1 +1,1 @@
-sudo RUST_LOG=trace cargo run -v -- --user=SBI-UG246275 -s sftp.swiss-backup02.infomaniak.com -p oiHN23VmoEcR
+sudo RUST_LOG=trace cargo run -v -- --user=<USER> -s <DESTINATION>.infomaniak.com -p <password>
